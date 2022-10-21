30968fa35ee7f98e4b326a3fe1f016f2819652a2
Антон Алексеев <gutolin12@yandex.ru>
2022-10-21 19:50:14 +0300

    Корректировка моделей, добавленны скрипты импорта из csv файла записи в бд

---
+++ api_yamdb/reviews/management/commands/import_category.py
@@ -0,0 +1,19 @@
+import csv
+from django.core.management.base import BaseCommand
+from reviews.models import Categories
+
+
+class Command(BaseCommand):
+    help = 'import category'
+
+    def handle(self, *args, **options):
+        with open('static/data/category.csv',
+                  encoding="utf-8-sig") as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',')
+            for row in csv_reader:
+                name = row['name']
+                slug = row['slug']
+
+                models = Categories(name=name, slug=slug)
+                models.save()
+        print("Импорт категорий завершен")
