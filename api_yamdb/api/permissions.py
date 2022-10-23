c78759e99149850deb2b2133474703f0d7950584
VegarusNik <mv-nikolay@vegarus.su>
2022-10-24 01:43:22 +0300

    Fix import permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,7 +2,6 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
-
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_admin
